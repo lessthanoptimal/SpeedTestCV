dbd9b15fde648962b13b565b811ba36df4ce1bdb
Peter Abeles <peter.abeles@gmail.com>
2019-03-02 16:34:05 -0800

    - fixed threads not being correctly configured in BoofCV - Added sketch of all the functions which will be compared

--- boofcv/src/main/java/speed/BenchmarkImageProcessing.java
+++ boofcv/src/main/java/speed/BenchmarkImageProcessing.java
@@ -2,15 +2,21 @@ package speed;
 
 import boofcv.abst.feature.detect.interest.ConfigGeneralDetector;
 import boofcv.abst.feature.detect.line.DetectLine;
+import boofcv.abst.filter.binary.BinaryContourFinder;
 import boofcv.abst.filter.binary.InputToBinary;
 import boofcv.abst.filter.blur.BlurFilter;
 import boofcv.abst.filter.derivative.ImageGradient;
+import boofcv.alg.feature.detect.edge.CannyEdge;
 import boofcv.alg.feature.detect.interest.GeneralFeatureDetector;
+import boofcv.alg.misc.ImageStatistics;
 import boofcv.concurrency.BoofConcurrency;
+import boofcv.factory.feature.detect.edge.FactoryEdgeDetectors;
 import boofcv.factory.feature.detect.interest.FactoryDetectPoint;
 import boofcv.factory.feature.detect.line.ConfigHoughFoot;
 import boofcv.factory.feature.detect.line.ConfigHoughPolar;
 import boofcv.factory.feature.detect.line.FactoryDetectLineAlgs;
+import boofcv.factory.filter.binary.FactoryBinaryContourFinder;
+import boofcv.factory.filter.binary.FactoryBinaryImageOps;
 import boofcv.factory.filter.binary.FactoryThresholdBinary;
 import boofcv.factory.filter.blur.FactoryBlurFilter;
 import boofcv.factory.filter.derivative.FactoryDerivative;
@@ -34,31 +40,35 @@ public class BenchmarkImageProcessing {
     public boolean concurrent=true;
 
     int radius = 5;
-    ConfigLength kerLenth = ConfigLength.fixed(radius*2+1);
+    ConfigLength kerLength = ConfigLength.fixed(radius*2+1);
 
     GrayU8 grayU8;
     GrayU8 outputU8;
+    GrayU8 binaryU8;
 
     GrayS16 output1_S16;
     GrayS16 output2_S16;
 
     // see python comments for these values
-    ConfigGeneralDetector configGoodFeats = new ConfigGeneralDetector(1000,2,0.0000001f);
+    ConfigGeneralDetector configGoodFeats = new ConfigGeneralDetector(-1,10,0.0000001f);
 
+    int[] histogram = new int[256];
 
     ConfigHoughPolar configHoughP = new ConfigHoughPolar(1,100,1,Math.PI/180.0,50,0);
     ConfigHoughFoot configHoughF = new ConfigHoughFoot(2,100,5,50,0);
 
     // use filter interface since it's easier to profile
-    BlurFilter<GrayU8> gaussianBlur = FactoryBlurFilter.gaussian(GrayU8.class,-1,5);
-    InputToBinary<GrayU8> threshMean = FactoryThresholdBinary.localMean(kerLenth,1.0,true,GrayU8.class);
-    ImageGradient<GrayU8, GrayS16> sobel = FactoryDerivative.sobel(GrayU8.class,GrayS16.class);
+    BlurFilter<GrayU8> gaussianBlur;
+    InputToBinary<GrayU8> threshMean;
+    ImageGradient<GrayU8, GrayS16> sobel;
 
     // OpenCV most likely implements just the weighted variant. That's likely because it's used for chessboard detection
-    GeneralFeatureDetector<GrayU8,GrayS16> goodFeats = FactoryDetectPoint.createShiTomasi(configGoodFeats,false,GrayS16.class);
-    GeneralFeatureDetector<GrayU8,GrayS16> goodFeatsW = FactoryDetectPoint.createShiTomasi(configGoodFeats,true,GrayS16.class);
-    DetectLine<GrayU8> houghPolar = FactoryDetectLineAlgs.houghPolar(configHoughP,GrayU8.class,GrayS16.class);
-    DetectLine<GrayU8> houghFoot = FactoryDetectLineAlgs.houghFoot(configHoughF,GrayU8.class,GrayS16.class);
+    GeneralFeatureDetector<GrayU8,GrayS16> goodFeats;
+    GeneralFeatureDetector<GrayU8,GrayS16> goodFeatsW;
+    DetectLine<GrayU8> houghPolar;
+    DetectLine<GrayU8> houghFoot;
+    CannyEdge<GrayU8,GrayS16> canny;
+    BinaryContourFinder contourA;
 
     @Setup
     public void setup() {
@@ -66,38 +76,55 @@ public class BenchmarkImageProcessing {
 
         grayU8 = UtilImageIO.loadImage("../data/chessboard_large.jpg",GrayU8.class);
         outputU8 = grayU8.createSameShape();
+        binaryU8 = grayU8.createSameShape();
 
         output1_S16 = new GrayS16(grayU8.width,grayU8.height);
         output2_S16 = new GrayS16(grayU8.width,grayU8.height);
+
+        // filters must be declared after USE_CONCURRENT has been set or else it won't stick
+        gaussianBlur = FactoryBlurFilter.gaussian(GrayU8.class,-1,5);
+        threshMean = FactoryThresholdBinary.localMean(kerLength,1.0,true,GrayU8.class);
+        sobel = FactoryDerivative.sobel(GrayU8.class,GrayS16.class);
+        goodFeats = FactoryDetectPoint.createShiTomasi(configGoodFeats,false,GrayS16.class);
+        goodFeatsW = FactoryDetectPoint.createShiTomasi(configGoodFeats,true,GrayS16.class);
+        houghPolar = FactoryDetectLineAlgs.houghPolar(configHoughP,GrayU8.class,GrayS16.class);
+        houghFoot = FactoryDetectLineAlgs.houghFoot(configHoughF,GrayU8.class,GrayS16.class);
+
+        canny = FactoryEdgeDetectors.canny(2,true, false, GrayU8.class, GrayS16.class);
+
+        contourA = FactoryBinaryContourFinder.linearExternal();
+
+        // TODO replace with image loaded from disk
+        threshMean.process(grayU8, binaryU8);
     }
 
-//    @Benchmark
-//    public void gaussianBlur() {
-//        gaussianBlur.process(grayU8, outputU8);
-//    }
-//
-//    @Benchmark
-//    public void threshMean() {
-//        threshMean.process(grayU8, outputU8);
-//    }
+    @Benchmark
+    public void gaussianBlur() {
+        gaussianBlur.process(grayU8, outputU8);
+    }
 
-//    @Benchmark
-//    public void sobel() {
-//        sobel.process(grayU8, output1_S16,output2_S16);
-//    }
+    @Benchmark
+    public void threshMean() {
+        threshMean.process(grayU8, outputU8);
+    }
 
-//    @Benchmark
-//    public void goodFeatures() {
-//        // the concurrent code here seems to be barely utilized
-//        sobel.process(grayU8, output1_S16,output2_S16);
-//        goodFeats.process(grayU8,output1_S16,output2_S16,null,null,null);
-//    }
-//
-//    @Benchmark
-//    public void goodFeaturesWeighted() {
-//        sobel.process(grayU8, output1_S16,output2_S16);
-//        goodFeatsW.process(grayU8,output1_S16,output2_S16,null,null,null);
-//    }
+    @Benchmark
+    public void sobel() {
+        sobel.process(grayU8, output1_S16,output2_S16);
+    }
+
+    @Benchmark
+    public void goodFeatures() {
+        // the concurrent code here seems to be barely utilized
+        sobel.process(grayU8, output1_S16,output2_S16);
+        goodFeats.process(grayU8,output1_S16,output2_S16,null,null,null);
+    }
+
+    @Benchmark
+    public void goodFeaturesWeighted() {
+        sobel.process(grayU8, output1_S16,output2_S16);
+        goodFeatsW.process(grayU8,output1_S16,output2_S16,null,null,null);
+    }
 
     @Benchmark
     public void houghPolar() {
@@ -107,6 +134,22 @@ public class BenchmarkImageProcessing {
     @Benchmark
     public void houghFoot() {
         houghFoot.detect(grayU8);
+    }
+
+    @Benchmark
+    public void canny() {
+        canny.process(grayU8,10,100,null);
+        // TODO tune so that the number of points matches opencv
+    }
+
+    @Benchmark
+    public void contourExternal() {
+        contourA.process(binaryU8);
+    }
+
+    @Benchmark
+    public void histogram() {
+        ImageStatistics.histogram(grayU8,0,histogram);
     }
 
 }
--- opencv/benchmark.py
+++ opencv/benchmark.py
@@ -5,44 +5,65 @@ import numpy as np
 
 num_trials = 10
 image_path = "../data/chessboard_large.jpg"
+
+# NOTE: OpenCV by RGB into Gray using a weighted average. BoofCV uses just the average
 img = cv2.imread(image_path,cv2.IMREAD_GRAYSCALE)
+
+# TODO replace with an image loaded from disk and thresholded
+thresh_img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 0)
 
 region_radius = 5
 region_width = region_radius*2+1
+
+# Leave comment out for official benchmarks
+# cv2.setNumThreads(0)
 
 def gaussianBlur():
     cv2.GaussianBlur(img, (region_width, region_width), 0)
 
 def gradientSobel():
-    cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=1)
-    cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=1)
+    # Example converted it to CV_64F, which caused a massive slow down. Keeping it integer
+    cv2.Sobel(img, cv2.CV_16S, 1, 0, ksize=1)
+    cv2.Sobel(img, cv2.CV_16S, 0, 1, ksize=1)
 
 def meanThresh():
     cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, region_width, 0)
 
+def computeHistogram():
+    cv2.calcHist([img],[0],None,[256],[0,256])
+    # TODO verify that histogram is similar
+
 def goodFeatures():
-    # For comparing the two algorithms quality of zero would be idea because chances are the meaning
-    # of quality is off by a scale factor. OpenCV doesn't allow that so a small value is used instead
-    cv2.goodFeaturesToTrack(img,1000,qualityLevel=0.000001,minDistance=2)
+    # Not sure if this is weighted or unweighted. Documentation uses the word "average" which would imply
+    # unweighted
+    cv2.goodFeaturesToTrack(img,0,qualityLevel=0.000001,minDistance=10,blockSize=21)
+    # TODO verify that a similar number of corners is returned
 
+def computeCanny():
+    # TODO tune to match number of points found in boofcv
+    cv2.Canny(img, 10, 100)
 
-# TODO SURF
+# SIFT and SURF are not included in the standard distribution of Python OpenCV due to legal concerns
+# Doing a custom build of OpenCV is beyond the scope scope of this benchmark since its only supposed to
+# include what's easily available
+# sift = cv2.xfeatures2d.SIFT_create()
+# def detectSift():
+#     sift.detectAndCompute(img, None)
 
-# TODO SIFT
-
+# TODO load an already thresholded image
 # TODO contour
 def contour():
     # Code in Validation Boof attempted to replicate the same behavior in both libraries. Configuration
     # values are taken from there.
-    # The algorithm in OpenCV and thw two contour algorithms in BoofCV operate internally very different
-    pass
-
-# TODO Canny - Skipped due to difficulty in configuring them the same way
+    # The algorithm in OpenCV and the two contour algorithms in BoofCV operate internally very different
+    cv2.findContours(thresh_img,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
+    # TODO verify similar results
 
 def houghLine():
     # OpenCV contains 3 Hough Line algorithms. CV_HOUGH_STANDARD is the closest to the variants included
     # in boofcv. The other OpenCV variants should have very different behavior based on their description
     cv2.HoughLines(img, rho=1, theta=np.pi/180, threshold=100)
+    # TODO verify similar results
 
 def benchmark( f ):
     times=[]
@@ -53,10 +74,13 @@ def benchmark( f ):
         times.append(t1-t0)
     return statistics.mean(times)*1000
 
-# print("Gaussian Blur   {:.1f} ms".format(benchmark(gaussianBlur)))
-# print("meanThresh      {:.1f} ms".format(benchmark(meanThresh)))
-# print("gradient sobel  {:.1f} ms".format(benchmark(gradientSobel)))
-# print("good features   {:.1f} ms".format(benchmark(goodFeatures)))
+print("Gaussian Blur   {:.1f} ms".format(benchmark(gaussianBlur)))
+print("meanThresh      {:.1f} ms".format(benchmark(meanThresh)))
+print("gradient sobel  {:.1f} ms".format(benchmark(gradientSobel)))
+print("histogram       {:.1f} ms".format(benchmark(computeHistogram)))
+print("canny           {:.1f} ms".format(benchmark(computeCanny)))
+print("contour         {:.1f} ms".format(benchmark(contour)))
+print("good features   {:.1f} ms".format(benchmark(goodFeatures)))
 print("hough polar     {:.1f} ms".format(benchmark(houghLine)))
 
 print()
